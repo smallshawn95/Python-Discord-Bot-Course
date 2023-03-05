2f6fa39c28762ad9841bcfdf43b1dccc2bf6654f
SmallShawn95 <120244412+smallshawn95@users.noreply.github.com>
2023-03-05 21:35:06 +0800

    Create 關鍵字.py

---
+++ Python Discord Bot 基礎教學/關鍵字.py
@@ -0,0 +1,25 @@
+# 導入Discord.py模組
+import discord
+
+# client是跟discord連接，intents是要求機器人的權限
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = intents)
+
+# 調用event函式庫
+@client.event
+# 當機器人完成啟動
+async def on_ready():
+    print(f"目前登入身份 --> {client.user}")
+
+@client.event
+# 當頻道有新訊息
+async def on_message(message):
+    # 排除機器人本身的訊息，避免無限循環
+    if message.author == client.user:
+        return
+    # 新訊息包含Hello，回覆Hello, world!
+    if message.content == "Hello":
+        await message.channel.send("Hello, world!")
+
+client.run("機器人的TOKEN")
