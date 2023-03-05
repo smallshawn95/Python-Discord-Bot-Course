0e865701997e64e5d93540d776f9e89ca0128f4a
SmallShawn95 <120244412+smallshawn95@users.noreply.github.com>
2023-03-05 21:32:33 +0800

    Create 前綴指令.py

---
+++ Python Discord Bot 基礎教學/前綴指令.py
@@ -0,0 +1,22 @@
+# 導入Discord.py模組
+import discord
+# 導入commands指令模組
+from discord.ext import commands
+
+# intents是要求機器人的權限
+intents = discord.Intents.all()
+# command_prefix是前綴符號，可以自由選擇($, #, &...)
+bot = commands.Bot(command_prefix = "%", intents = intents)
+
+@bot.event
+# 當機器人完成啟動
+async def on_ready():
+    print(f"目前登入身份 --> {bot.user}")
+
+@bot.command()
+# 輸入%Hello呼叫指令
+async def Hello(ctx):
+    # 回覆Hello, world!
+    await ctx.send("Hello, world!")
+
+bot.run("機器人的TOKEN")
