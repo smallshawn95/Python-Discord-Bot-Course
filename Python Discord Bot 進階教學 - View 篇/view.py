d34c1e302d5b58b4b3bd6322d10154ff8d4ed278
SmallShawn95 <120244412+smallshawn95@users.noreply.github.com>
2023-07-15 22:29:54 +0800

    2023-07-15 Update view.py

---
+++ Python Discord Bot 進階教學 - View 篇/view.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from core.classes import Cog_Extension
+
+# 宣告一個 ViewClass 類別，繼承 discord.ui.View
+class ViewClass(discord.ui.View):
+    def __init__(self, timeout: float | None = 180):
+        super().__init__(timeout = timeout)
+        # 添加一個 Button 到 ViewClass 中
+        self.add_item(discord.ui.Button(label = "Button"))
+
+class View(Cog_Extension):
+    @app_commands.command(name = "view_base", description = "簡單 View 範例")
+    async def view_base(self, interaction: discord.Interaction):
+        # 創建一個 View，並設置 30 秒超時
+        view = discord.ui.View(timeout = 30)
+        # 添加一個 Button 到 View 中
+        view.add_item(discord.ui.Button(label = "Button"))
+        await interaction.response.send_message(view = view)
+
+    @app_commands.command(name = "view_class", description = "Class 版本 View 範例")
+    async def embed_class(self, interaction: discord.Interaction):
+        # 創建一個 ViewClass 類別，並設置 30 秒超時
+        view = ViewClass(timeout = 30)
+        await interaction.response.send_message(view = view)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(View(bot))
