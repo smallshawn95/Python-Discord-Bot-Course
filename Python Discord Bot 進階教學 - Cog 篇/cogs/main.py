263920485fee2103be0f3871aa1e0803ed9c005a
SmallShawn95 <120244412+smallshawn95@users.noreply.github.com>
2023-08-24 15:00:33 +0800

    2023-08-24 Update main.py

--- Python Discord Bot 進階教學 - Cog 篇/cogs/main.py
+++ Python Discord Bot 進階教學 - Cog 篇/cogs/main.py
@@ -1,23 +1,24 @@
 import discord
 from discord.ext import commands
-# 導入core資料夾中的自寫模組
-from core.classes import Cog_Extension
 
-# 繼承Cog_Extension的self.bot物件
-class Main(Cog_Extension):
+# 定義名為 Main 的 Cog
+class Main(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
     # 前綴指令
     @commands.command()
-    async def Hello(self, ctx):
+    async def Hello(self, ctx: commands.Context):
         await ctx.send("Hello, world!")
-      
+
     # 關鍵字觸發
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         if message.author == self.bot.user:
             return
         if message.content == "Hello":
             await message.channel.send("Hello, world!")
 
-# 載入cog中
-async def setup(bot):
+# Cog 載入 Bot 中
+async def setup(bot: commands.Bot):
     await bot.add_cog(Main(bot))
